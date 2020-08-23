73e2db5c01492719b5066118dc5e9d375beb6223
rickymwethera <rickymwethera@gmail.com>
2020-08-24 01:26:05 +0300

    added quote class

---
+++ src/app/quote.ts
@@ -0,0 +1,12 @@
+export class Quote {
+
+    showDescription= false;
+    
+      constructor(
+        public id: number,
+        public name: string,
+        public description: string,
+        public submitter: string,
+        public completeDate: Date) {
+    }
+      }
